ffb8d9b66ceefa85207e908e757298db74dfc393
dev7 <dev7@gmail.com>
2024-09-04 11:31:07 +0530

    TL commited

--- src/com/nt/test1/Test1.java
+++ src/com/nt/test1/Test1.java
@@ -13,4 +13,7 @@ public static void main(String[] args) {
 	System.out.println("this is main method");
 	System.out.println("********************");
 }
+void m2() {
+	System.out.println("Test1.m2()");
+}
 }
