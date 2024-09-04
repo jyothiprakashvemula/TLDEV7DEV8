d23a2516d15fd2ff84b58a43a6a27719cf59cec2
dev7 <dev7@gmail.com>
2024-09-04 09:39:58 +0530

    dev8 added the code to Test2 java class

--- src/com/nt/test2/Test2.java
+++ src/com/nt/test2/Test2.java
@@ -1,5 +1,10 @@
 package com.nt.test2;
 
-public class Test2 {
+import com.nt.test1.Test1;
 
+public class Test2 {
+public static void main(String[] args) {
+	Test1 t1=new Test1();
+	System.out.println(t1.datemethod());
+}
 }
